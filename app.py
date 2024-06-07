e2dcf4db7be49a6aed2b486da1380cdc68931d79
Cunninger <1803912219@qq.com>
2024-06-07 11:01:17 +0800

    test:15 site-extension

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+    return render_template('combined.html')
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
