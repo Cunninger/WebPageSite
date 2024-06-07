9a26963171c48d4c42b2bdd54f2b4b2efe8a7411
Cunninger <1803912219@qq.com>
2024-06-07 08:20:55 +0800

    test:4

--- app.py
+++ app.py
@@ -2,9 +2,120 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+websites = [
+    {
+        'img_src': 'images/logo.png',
+        'alt_text': 'Example',
+        'title': 'Example',
+        'description': '这是一个示例平台的描述'
+    },
+
+    {
+        'img_src': 'images/logo.png',
+        'alt_text': 'Example',
+        'title': 'Example',
+        'description': '这是一个示例平台的描述'
+    },
+
+    {
+        'img_src': '/images/logo.png',
+        'alt_text': 'Example',
+        'title': 'Example',
+        'description': '这是一个示例平台的描述'
+    },
+
+    {
+        'img_src': '/images/logo.png',
+        'alt_text': 'Example',
+        'title': 'Example',
+        'description': '这是一个示例平台的描述'
+    },
+
+    {
+        'img_src': '/images/logo.png',
+        'alt_text': 'Example',
+        'title': 'Example',
+        'description': '这是一个示例平台的描述'
+    },
+
+    {
+        'img_src': '/images/logo.png',
+        'alt_text': 'Example',
+        'title': 'Example',
+        'description': '这是一个示例平台的描述'
+    },
+
+    {
+        'img_src': '/images/logo.png',
+        'alt_text': 'Example',
+        'title': 'Example',
+        'description': '这是一个示例平台的描述'
+    },
+
+    {
+        'img_src': '/images/logo.png',
+        'alt_text': 'Example',
+        'title': 'Example',
+        'description': '这是一个示例平台的描述'
+    },
+
+    {
+        'img_src': '/images/logo.png',
+        'alt_text': 'Example',
+        'title': 'Example',
+        'description': '这是一个示例平台的描述'
+    },
+
+    {
+        'img_src': '/images/logo.png',
+        'alt_text': 'Example',
+        'title': 'Example',
+        'description': '这是一个示例平台的描述'
+    },
+
+    {
+        'img_src': '/images/logo.png',
+        'alt_text': 'Example',
+        'title': 'Example',
+        'description': '这是一个示例平台的描述'
+    },
+
+    {
+        'img_src': '/images/logo.png',
+        'alt_text': 'Example',
+        'title': 'Example',
+        'description': '这是一个示例平台的描述'
+    },
+
+    {
+        'img_src': '/images/logo.png',
+        'alt_text': 'Example',
+        'title': 'Example',
+        'description': '这是一个示例平台的描述'
+    },
+
+    {
+        'img_src': '/images/logo.png',
+        'alt_text': 'Example',
+        'title': 'Example',
+        'description': '这是一个示例平台的描述'
+    },
+
+    {
+        'img_src': '/images/logo.png',
+        'alt_text': 'Example',
+        'title': 'Example',
+        'description': '这是一个示例平台的描述'
+    },
+
+    # 添加更多网站信息...
+]
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', websites=websites)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
