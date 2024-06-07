fd385266e5ae5e69221af692c805978bd45614c6
Cunninger <1803912219@qq.com>
2024-06-07 11:31:18 +0800

    test:17 site-extension-back

--- build.py
+++ build.py
@@ -1,6 +1,5 @@
 import os
 import shutil
-import glob
 
 if __name__ == '__main__':
     if os.path.exists('build'):
@@ -8,9 +7,4 @@ if __name__ == '__main__':
 
     os.mkdir('build')
     shutil.copytree('static', 'build/static')
-
-    # Copy all HTML files from templates to build
-    for html_file in glob.glob('templates/*.html'):
-        shutil.copy(html_file, 'build/')
-
-    shutil.copy('CNAME', 'build/CNAME')  # Add this line to copy CNAME file
+    shutil.copy('templates/index.html', 'build/index.html')
